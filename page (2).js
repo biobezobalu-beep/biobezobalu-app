f90304f4677e344de1a9fba5e8a65dc308583215
biobezobalu-beep <biobezobalu@gmail.com>
2026-07-28 23:31:41 +0200

    Delete page (2).js

--- page (2).js
+++
@@ -1,3 +0,0 @@
-import './globals.css';
-export const metadata={title:'BioBezObalu Club',description:'Předprodej limitovaných čokolád se slevou 15 %'};
-export default function RootLayout({children}){return <html lang="cs"><body>{children}</body></html>}
